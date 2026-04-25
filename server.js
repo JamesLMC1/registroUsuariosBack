53b7d6c2f0e47248c94a4752a89776e4318b504c
James <jamesleonardo230@gmail.com>
2026-04-24 19:27:36 -0500

    Cambios

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ const swaggerJsdoc = require("swagger-jsdoc");
 
 const app = express();
 app.use(express.json());
-
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 // -------------------------------------------------------------
 // 1. CLIENTE SUPABASE
 //    Variables de entorno requeridas (configurar en Railway):
